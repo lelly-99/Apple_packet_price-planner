ac9a2e1ff0ee828729664370cf8b0e1f4b11491e
progress <leswifil@gmail.com>
2023-05-26 17:37:00 +0200

    factory functions done

--- factory function/appleFunction.js
+++ factory function/appleFunction.js
@@ -1,37 +1,66 @@
-function applePacketPricePlanner(){
+function applePacketPricePlanner() {
 
     var costforBoxOfApples = 0
     var costPricePerApple = 0
     var costPricePerPacket = 0
     var reccommendedPacketPrice = 0
+    var packetSizeApplesAreSoldIn = 0
+    var costforBoxOfApples = 0
+    var applesInTheBox = 0
+    var recomprofit = 0
 
-    function getnumberOfPackets(){
-        numberOfPackets = costforBoxOfApples/packetSizeApplesAreSoldIn
+
+    function setcostforBoxOfApples(box) {
+        costforBoxOfApples = box
+        return costforBoxOfApples
+    }
+
+    function setpacketSizeApplesAreSoldIn(packet) {
+        packetSizeApplesAreSoldIn = packet
+        return packetSizeApplesAreSoldIn
+    }
+
+    function setapplesInTheBox(apples) {
+        applesInTheBox = apples
+        return applesInTheBox
+    }
+
+    function setprofit(profit) {
+        recomprofit = profit
+        return recomprofit
+    }
+
+    function getnumberOfPackets() {
+        numberOfPackets = costforBoxOfApples / packetSizeApplesAreSoldIn
         return numberOfPackets.toFixed(2)
     }
 
-    function getcostPricePerApple(){
-        costPricePerApple = costforBoxOfApples/applesInTheBox
+    function getcostPricePerApple() {
+        costPricePerApple = costforBoxOfApples / applesInTheBox
         return costPricePerApple.toFixed(2)
     }
 
-    function getcostPricePerPacket(){
-        costPricePerPacket = costforBoxOfApples/packetSizeApplesAreSoldIn
+    function getcostPricePerPacket() {
+        costPricePerPacket = costforBoxOfApples / packetSizeApplesAreSoldIn
         return costPricePerPacket.toFixed(2)
     }
 
-    function getReccommendedPacketPrice(){
-        reccommendedPacketPrice = (costforBoxOfApples/packetSizeApplesAreSoldIn)*profit
+    function getReccommendedPacketPrice() {
+        reccommendedPacketPrice = (costforBoxOfApples / packetSizeApplesAreSoldIn) * recomprofit
         return reccommendedPacketPrice.toFixed(2)
     }
     return {
         getnumberOfPackets,
         getcostPricePerApple,
         getcostPricePerPacket,
-        getReccommendedPacketPrice 
+        getReccommendedPacketPrice,
+        setpacketSizeApplesAreSoldIn,
+        setcostforBoxOfApples,
+        setapplesInTheBox,
+        setprofit,
     }
 }
 
 
 
-    
+
--- widget/apples.js
+++ widget/apples.js
@@ -11,18 +11,20 @@ var costPricePerApple = document.querySelector(".costPricePerApple")
 var costPricePerPacket = document.querySelector(".costPricePerPacket")
 var reccommendedPacketPrice = document.querySelector(".reccommendedPacketPrice")
 
-
+var applePlanner = applePacketPricePlanner()
 //event listener
-calculateButton.addEventListener("click", function(){
+calculateButton.addEventListener("click", function () {
     var costforBoxOfApples = Number(costPriceForBoxOfApples.value)
     var applesInTheBox = Number(numberOfApplesInTheBox.value)
     var packetSizeApplesAreSoldIn = Number(thePacketSizeApplesAreSoldIn.value)
     var profit = Number(requiredProfit.value)
 
-    costPricePerApple.innerHTML = (costforBoxOfApples/applesInTheBox).toFixed(2)
-    costPricePerPacket.innerHTML = (costforBoxOfApples/packetSizeApplesAreSoldIn).toFixed(2)
-    reccommendedPacketPrice.innerHTML = ((costforBoxOfApples/packetSizeApplesAreSoldIn)*profit).toFixed(2)
-    numberOfPackets.innerHTML = (applesInTheBox/packetSizeApplesAreSoldIn).toFixed(2)
+    if (costPriceForBoxOfApples && applesInTheBox && packetSizeApplesAreSoldIn && profit) {
+        costPricePerApple.innerHTML = (costforBoxOfApples / applesInTheBox).toFixed(2)
+        costPricePerPacket.innerHTML = (costforBoxOfApples / packetSizeApplesAreSoldIn).toFixed(2)
+        reccommendedPacketPrice.innerHTML = ((costforBoxOfApples / packetSizeApplesAreSoldIn) * profit).toFixed(2)
+        numberOfPackets.innerHTML = (applesInTheBox / packetSizeApplesAreSoldIn).toFixed()
+    }
 })
 
 
